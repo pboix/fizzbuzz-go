eb215d1244b500f5dfeacf086d65d65a295975df
pau <pau@typeform.com>
2017-02-10 11:40:23 +0100

    Added ordered sorting on map

--- fizzbuzzrazz.go
+++ fizzbuzzrazz.go
@@ -7,6 +7,7 @@ import (
   "strconv"
   "bytes"
   "strings"
+  "sort"
 )
 
 var conditions = map[int]string { 3: "Fizz", 5: "Buzz", 7: "Razz" }
@@ -14,11 +15,16 @@ var buffer bytes.Buffer
 
 func fizzbuzzrazz(number int) string {
   buffer.Reset()
-  for key, value := range conditions {
-    if number % key == 0 {
-      buffer.WriteString(value)
-    }
+  var keys []int
+  for k := range conditions {
+    keys = append(keys, k)
   }
+    sort.Ints(keys)
+    for  _, k := range keys {
+      if number % k == 0 {
+        buffer.WriteString(conditions[k])
+      }
+    }
 
   if buffer.String() == "" {
     return strconv.Itoa(number) + " is not divisible by any of the options!"
@@ -32,8 +38,8 @@ func main() {
   fmt.Print("Welcome to the FizzBuzz challenge! Please, enter a number: ")
   text, _ := reader.ReadString('\n')
   number, err := strconv.Atoi(strings.TrimSpace(text))
-  if err != nil {
-    fmt.Println("This is not a number...")
+  if err != nil || number == 0 {
+    fmt.Println("You better enter a valid number...")
   } else {
     fmt.Println(fizzbuzzrazz(number))
   }
